755622f524b9ad6b148ac2e9cee5efd9ac9b9175
hirenkhambhayta7 <hirenkhambhayta7@gmail.com>
2026-04-17 14:22:51 +0530

    database chnage

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,26 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://satvik.de', 'https://www.satvik.de', 'http://localhost:8080'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
